0a40be63cd1d466f32f87df924b272fcd5afe209
Jorge Garcia Regueiro <97844407+JGarciaRegueiro@users.noreply.github.com>
2023-02-26 19:05:19 +0100

    Delete Grupo04_Actividad5 directory

--- Grupo04_Actividad5/src/main/java/com/jorge/exampleRest/modelo/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jorge.exampleRest.modelo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jorge.exampleRest.modelo.entities.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
-}
--- Grupo04_Actividad5/src/main/java/com/jorge/exampleRest/modelo/repository/ComercialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jorge.exampleRest.modelo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jorge.exampleRest.modelo.entities.Comercial;
-
-public interface ComercialRepository extends JpaRepository<Comercial, Integer>{
-
-}
